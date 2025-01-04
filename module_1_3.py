a0666eda941f53947b9ea9bd7f881ffc68413a0f
Amaru-droid <amarufine@icloud.com>
2025-01-04 17:35:27 +0400

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = ("Beloludtsev Alexey")
+print ('Name : ' ,name, type(name))
+age = 28
+print ('Age :', age, type(age),)
+new_age = 1
+print ('New age : ' , new_age+age, type(new_age))
+is_student = 'True'
+print ('Is Student : ' , is_student, type(is_student))
