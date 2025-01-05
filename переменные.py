3b9ecbf0c83b61c0e572a6fe4d8e25be90aee912
Amaru-droid <amarufine@icloud.com>
2025-01-05 13:46:31 +0400

    Add files via upload

---
+++ переменные.py
@@ -0,0 +1,11 @@
+name = ("Белослудцев Алексей Практическое задание по теме Переменные")
+print ('Name : ' ,name, )
+home_work = 12
+print ('Количество  выполненых ДЗ - ', home_work)
+used_hours = 1.5
+print ("Количество затраченных часов - ", used_hours,)
+cours_name = 'python'
+print ('Название курса - ', cours_name)
+used_hours_for_one_home_work = used_hours / home_work
+print ('Время потраченное на одно задание - ',used_hours_for_one_home_work)
+print ('Курс:', cours_name , ', всего задач:' , home_work , ',затрачено часов:' , used_hours, ',среднее время выполнения:' , used_hours_for_one_home_work)
